e81046e924f23dca45050129cc76349eff1a4fd5
Bernardo-07 <84275344+Bernardo-07@users.noreply.github.com>
2023-06-03 03:16:29 +0000

    Posicionamento v1

--- .ccls-cache/@home@runner@WarShip/main.c
+++ .ccls-cache/@home@runner@WarShip/main.c
@@ -3,8 +3,9 @@
 
 void tabuleiro(){
   int i, j;
-  printf("   1   2   3   4   5\n");
+  printf(" |   | 1 | 2 | 3 | 4 | 5 |\n");
   for(i=0; i<5; i++){
+    printf(" | %d",i+1);
     for(j=0; j<5; j++){
       printf(" | .");
     }
