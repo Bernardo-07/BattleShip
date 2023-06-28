e0d8acc98fc974d01930165089c35c436aec2b5d
Bernardo-07 <84275344+Bernardo-07@users.noreply.github.com>
2023-06-28 17:40:28 +0000

    Correção no Tabuleiro

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void tabuleiro(int tab[5][5]){
+void tabuleiro_inicial(){
+   int i, j;
+  printf(" | 0 | 1 | 2 | 3 | 4 | 5 |\n");
+  for(i=0; i<5; i++){
+    printf(" | %d",i+1);
+    for(j=0; j<5; j++){
+      printf(" | .");
+    }
+    printf(" |\n");
+  }
+}
+
+void tabuleiro_atualizado(int tab[5][5]){
   int i, j;
   printf(" | 0 | 1 | 2 | 3 | 4 | 5 |\n");
   for(i=0; i<5; i++){
@@ -9,11 +21,11 @@ void tabuleiro(int tab[5][5]){
     for(j=0; j<5; j++){
       if (tab[i][j] == 1){
         printf(" | X");
-      }
-      if (tab[i][j] == 0){
+      }else if (tab[i][j] == -1){
         printf(" | *");
+      }else{
+        printf(" | .");
       }
-      printf(" | .");
     }
     printf(" |\n");
   }
@@ -63,28 +75,37 @@ void posicionamento(int n[3][2]){
 int main(){
   
   int navios1[3][2], navios2[3][2];
-  int tabuleiro1[5][5], tabuleiro2[5][5], inicial[5][5];
+  int tabuleiro1[5][5];
+  int tabuleiro2[5][5];
   int turno = 1;
   int tiro[2];
-  int i, x, y;
+  int i, x, y, j, k;
   int pontos1=0, pontos2=0;
   int acerto=0; //acerto foi definido como falso
   char vazio;
   
-  tabuleiro(inicial);
+  tabuleiro_inicial();
   printf("\nJogador 1, digite as coordenadas dos seus três navios:\n");
   posicionamento(navios1);
   
-  tabuleiro(inicial);
+  tabuleiro_inicial();
   printf("\nJogador 2, digite as coordenadas dos seus três navios:\n");
   posicionamento(navios2);
+
+  for(j=0; j<5; j++){
+    for(k=0; k<5; k++){
+      tabuleiro1[j][k] = 0;
+      tabuleiro2[j][k] = 0;
+    }
+  }
+  //atribuimos uma matriz nula para os tabuleiros
 
   do{
     switch(turno){
       case 1:
         system("clear");
         printf("\nVez do jogador 1:\n");
-        tabuleiro(tabuleiro1);
+        tabuleiro_atualizado(tabuleiro1);
       
         printf("Linha (1-5): ");
         scanf("%d",&tiro[0]);
@@ -98,13 +119,13 @@ int main(){
             //se o jogador acertar as coordenadas de algum navio inimigo então sua contagem de pontos aumenta e a variável acerto fica definida como verdadeira
           }
         }
-        x = tiro[0];
-        y = tiro[1];
+        x = tiro[0]-1;
+        y = tiro[1]-1;
         if (acerto == 1){
           tabuleiro1[x][y] = 1;
           printf("\nVocê acertou o tiro!, Pontos = %d",pontos1);
         }else{
-          tabuleiro1[x][y] = 0;
+          tabuleiro1[x][y] = -1;
           printf("\nVocê errou o tiro..., Pontos = %d",pontos1);
         }
         printf("\nDigite um ponto '.' e dê enter para continuar\n");
@@ -116,7 +137,7 @@ int main(){
       case 2:
         system("clear");
         printf("\nVez do jogador 2:\n");
-        tabuleiro(tabuleiro2);
+        tabuleiro_atualizado(tabuleiro2);
       
         printf("Linha (1-5): ");
         scanf("%d",&tiro[0]);
@@ -129,13 +150,13 @@ int main(){
             pontos2++;
           }
         }
-        x = tiro[0];
-        y = tiro[1];
+        x = tiro[0]-1;
+        y = tiro[1]-1;
         if (acerto == 1){
           tabuleiro2[x][y] = 1;
           printf("Você acertou o tiro!, Pontos = %d",pontos2);
         }else{
-          tabuleiro2[x][y] = 0;
+          tabuleiro2[x][y] = -1;
           printf("Você errou o tiro..., Pontos = %d",pontos2);
         }
         printf("\nDigite algo e dê enter para continuar\n");
@@ -148,7 +169,6 @@ int main(){
   
   return 0;
 }
-//preciso checar se o usuário digitou coordenadas que ele já tinha digitado antes
-//preciso atualizar o tabuleiro em tempo real
-//preciso dar um jeito de trancar o tiro acertado,para que não conte pontos para o jogador que der o tiro no barco que já tinha afundado
 //adicionar a mensagem para o vencedor
+//preciso dar um jeito de trancar o tiro acertado,para que não conte pontos para o jogador que der o tiro no barco que já tinha afundado
+//preciso checar se o usuário digitou coordenadas que ele já tinha digitado antes
